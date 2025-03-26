7e4dee8a9c622d465aa5236f4ed6ff00e431ddab
Shashank-1K <96000384+Shashank-1K@users.noreply.github.com>
2025-03-26 14:33:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
+    callbackURL: "https://letter-creator-server.onrender.com/auth/google/callback",
     scope: [
       'profile', 
       'email',
