c36712f8f1aa8a7220453434e53b3601ffc5ddb3
Shashank-1K <96000384+Shashank-1K@users.noreply.github.com>
2025-03-26 10:44:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,13 +20,13 @@ app.use(passport.initialize());
 const JWT_SECRET = process.env.JWT_SECRET;
 
 // Add this to your main server file (index.js or server.js)
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/build')));
     
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-    });
-  }
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+//     });
+//   }
 
 // Google OAuth Configuration
 passport.use(new GoogleStrategy({
